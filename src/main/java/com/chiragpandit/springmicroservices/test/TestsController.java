05f2624904a01a9cda3831e710d32bc599b3d452
chirag.p <chirag.p@loginextsolutions.com>
2018-05-01 00:06:41 +0530

    commit

--- src/main/java/com/chiragpandit/springmicroservices/test/TestsController.java
+++ src/main/java/com/chiragpandit/springmicroservices/test/TestsController.java
@@ -4,28 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestsController {
-/*
-	@Autowired
-	TestsRepository testRepository;*/
-	
-	List<Test> tests = new ArrayList<Test>(
-			Arrays.asList(
-					new Test("com.hello","test1", "Y","3", "DataBin"),
-					new Test("com.hello","test2", "Y","3", "DataBin"),
-					new Test("com.hello","test3", "Y","3", "DataBin"),
-					new Test("com.hello","test4", "Y","3", "DataBin"),
-					new Test("com.hello","test5", "Y","3", "DataBin")
-					));
-	
+
 	@RequestMapping ("/tests")
-	public List <Test> getAllTests (){
-		return tests;
+	public String getAllTests (){
+		return "Hello";
 		/*List <Test> tests = new ArrayList<Test>();
 		Iterable <Test> testIterator = testRepository.findAll();
 		for (Test test : testIterator) 
--- src/main/java/com/chiragpandit/springmicroservices/test/TestsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chiragpandit.springmicroservices.test;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestsRepository extends MongoRepository<Test, String>
-{
-
-}
